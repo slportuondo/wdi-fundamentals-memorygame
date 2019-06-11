12b733e6afd146245fb4be537426a43fc59428f0
Bat <Sebastian@Bat.local>
2019-06-11 18:17:27 -0500

    Added game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,9 @@
+console.log("Up and running!");
+
+var card1 = 'queen';
+var card2 = 'queen';
+var card3 = 'king';
+var card4 = 'king';
+
+console.log("User flipped " + card1);
+console.log("User flipped " + card2);
